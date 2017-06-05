d82647ff0ab66757119e899b8b8b53696672e9a6
Cshion <aaron.stbn@gmail.com>
2017-06-05 13:38:51 -0500

    src: Integrando a slack

--- index.js
+++ index.js
@@ -1,3 +1,40 @@
-const data = require("./data.json");
+"use strict";
+const Operations = require("./operations");
+const app        = require("express")();
+const bodyParser = require("body-parser");
+const moment     = require('moment-timezone');
 
-console.log(data);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+
+app.post("/",function(req,res){
+  let text       = req.body["text"];
+  
+  if(!text){
+    return res.send("");
+  }
+
+  let date = moment().tz("America/Lima").format("HH:mm"); 
+  let llegada = Operations.obtenerProximaLlegada(text,"semana",date);
+  let salida  = Operations.obtenerProximaSalida(text,"semana",date);
+
+  if(!llegada || !salida){
+    return res.send("");
+  }
+
+  let text_result = "Proxima llegada: " + llegada +"\n"+ "Proxima salida: "+salida ;
+
+  let result     = {
+    color:"good",
+    text:text_result,
+    mrkdwn_in:["text"],
+    response_type: "in_channel",
+  }
+
+  return res.json(result)
+});
+
+app.listen(3000,function(){
+  console.log("Bot corriendo en 3000");
+});
